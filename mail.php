863a2adc0e284548e6fe7fe6866f08ab43f2871c
RichyMalicia <97110478+RichyMalicia@users.noreply.github.com>
2023-02-01 13:03:23 -0300

    first commit

---
+++ mail.php
@@ -0,0 +1,33 @@
+<?php
+
+require("vendor/autoload.php");
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+function sendMail($subject, $body, $email, $name, $html = false) {
+
+    // Configuración inicial de nuestro servidor de correos
+    $phpmailer = new PHPMailer();
+    $phpmailer->isSMTP();
+    $phpmailer->Host = 'smtp.gmail.com';
+    $phpmailer->SMTPAuth = true;
+    $phpmailer->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $phpmailer->Port = 465;
+    $phpmailer->Username = 'jorgeserviciosinmobiliarios@gmail.com';
+    $phpmailer->Password = 'yphvdqcwjalrqxar';
+
+    //  Añadiendo destinatarios
+    $phpmailer->setFrom($email, $name);
+    $phpmailer->addAddress($email, $name); 
+
+    // Definiendo el contenido de mi email
+    $phpmailer->isHTML($html);                                  //Set email format to HTML
+    $phpmailer->Subject = $subject;
+    $phpmailer->Body    = $body;
+
+    // Mandar el correo
+    $phpmailer->send();
+    
+}
+
+?>
